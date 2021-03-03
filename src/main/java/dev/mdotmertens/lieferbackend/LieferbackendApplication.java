e2d02f17c0bc744e68f5383f3487f534cf84c093
Maurice Mertens <maurice.mertens95@googlemail.com>
2021-03-03 11:38:26 +0100

    Initial Commit

---
+++ src/main/java/dev/mdotmertens/lieferbackend/LieferbackendApplication.java
@@ -0,0 +1,13 @@
+package dev.mdotmertens.lieferbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LieferbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LieferbackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/mdotmertens/lieferbackend/LieferbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mdotmertens.lieferbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LieferbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
